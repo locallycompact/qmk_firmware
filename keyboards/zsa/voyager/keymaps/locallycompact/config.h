4c0ea846e57dd74e0cbd7e7d240b75af0fd18bab
Daniel Firth <dan.firth@homotopic.tech>
2024-12-30 12:06:55 +0000

    zsa/voyager: locallycompact: init

---
+++ keyboards/zsa/voyager/keymaps/locallycompact/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#undef MOUSEKEY_DELAY
+#undef MOUSEKEY_INTERVAL
+#undef MOUSEKEY_MAX_SPEED
+#undef MOUSEKEY_TIME_TO_MAX
+
+#define MOUSEKEY_INTERVAL           10
+#define MOUSEKEY_DELAY              1
+#define MOUSEKEY_TIME_TO_MAX        40
+#define MOUSEKEY_MAX_SPEED          7
+#define MOUSEKEY_WHEEL_DELAY        400
+
+#define TAPPING_TERM 200
--- keyboards/zsa/voyager/keymaps/locallycompact/keymap.c
+++ keyboards/zsa/voyager/keymaps/locallycompact/keymap.c
@@ -20,9 +20,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                      _______, _______,    _______, KC_0
     ),
     [2] = LAYOUT(
-        RM_TOGG, QK_KB,   RM_NEXT, RGB_M_P, RM_VALD, RM_VALU,                      _______, _______, _______, _______, _______, QK_BOOT,
+        RGB_TOG, QK_KB,   RGB_MOD, RGB_M_P, RGB_VAD, RGB_VAI,                      _______, _______, _______, _______, _______, QK_BOOT,
         _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,                      KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, _______,
-        _______, KC_MPRV, KC_MNXT, KC_MSTP, KC_MPLY, _______,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
+        _______, KC_MPRV, KC_MNXT, KC_MSTP, KC_MPLY, _______,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, TO(4),
         _______, _______, _______, _______, _______, _______,                      _______, C(S(KC_TAB)), C(KC_TAB), _______, _______, _______,
                                                      _______, _______,    _______, _______
     ),
@@ -33,6 +33,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______,                     _______, C(S(KC_TAB)), C(KC_TAB), _______, _______, _______,
                                                      KC_MS_BTN1, KC_MS_BTN2,    _______, _______
     ),
-
+    [4] = LAYOUT(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_DEL, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_BSPC,KC_A, KC_S, KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RSFT_T(KC_QUOT),
+        KC_LGUI, KC_Z,KC_X, KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMMA,KC_DOT,  RALT_T(KC_SLSH), KC_RCTL,
+                                KC_LSFT, KC_LCTL,           KC_TAB, LT(2,KC_ENT)
+    ),
 
 };
